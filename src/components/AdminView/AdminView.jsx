268fbe1e026877c6ace96d983168a03140dd09a2
Diego Baeza <diegob12@live.com>
2022-07-17 18:27:55 -0500

    add packages, admin page component, starting routes

---
+++ src/components/AdminView/AdminView.jsx
@@ -0,0 +1,14 @@
+function AdminView() {
+    return (
+        <>
+            <div>
+                <p>Feedback Results</p>
+            </div>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+export default AdminView;
